af65369ba3a71b805b4878054b58b283d3fcc686
Lucas Ricarte <lucasrrt@gmail.com>
2018-09-27 01:47:03 -0300

    almost

--- image/main.py
+++ image/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import math
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
@@ -21,7 +22,7 @@ print inverse
 multiplication = np.matmul(transformation, inverse)
 print multiplication
 
-support = Polygon([(264,103),(517,172),(520,388),(262,397)])
+support = Polygon([(103,264),(172,517),(388,520),(397,262)])
 
 ambient_height, ambient_width = new_ambient.shape[:2]
 painting_height, painting_width = painting.shape[:2]
@@ -34,11 +35,27 @@ for j in range(0, ambient_height - 1):
     for i in range(0, ambient_width - 1):
         #print support.contains(point)
         #new_ambient[i, j] = painting[i, j]
-        point = Point(i, j)
+        point1 = Point(j, i)
         #print (i,j)
-        new_ambient[j,i] = [255,255,255]
-        if(support.contains(point)):
-            new_ambient[j,i] = [0, 0, 0]
+        #new_ambient[j,i] = [255,255,255]
+        correspondent = np.matmul(inverse, [j, i, 1])
+        #print correspondent
+        point = Point(correspondent[1]/correspondent[2], correspondent[0]/correspondent[2])
+        if(support.contains(point1)):
+            #print point
+            print math.floor(point.x), math.floor(point.y)
+            x = int(math.floor(point.x)) - 242
+            y = int(math.floor(point.y)) + 214
+            if(x < 0):
+                x = 0
+            elif x > 504:
+                x = 504
+            if y < 0:
+                y = 0
+            elif y > 411:
+                y = 411
+            print x, y
+            new_ambient[j,i] = painting[y, x] 
 
 cv2.imshow('original ambient', ambient)
 cv2.imshow('painting', painting)
