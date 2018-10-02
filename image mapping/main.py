21b40496cd7f5782bf5006374b7190936be2ea17
Lucas Ricarte <lucasrrt@gmail.com>
2018-10-02 14:37:01 -0300

    refactoring image sampling

--- image mapping/main.py
+++ image mapping/main.py
@@ -9,28 +9,19 @@ ambient = cv2.resize(ambient, (0,0), fx=0.25, fy=0.25)
 new_ambient = ambient.copy()
 
 painting = cv2.imread('painting.jpg',1)
-#painting = cv2.resize(painting, (0,0), fx=0.5, fy=0.5)
 
 ambient_height, ambient_width = new_ambient.shape[:2]
 painting_height, painting_width = painting.shape[:2]
 print ambient_height, ambient_width
 print painting_height, painting_width
 
-#transformation = np.array([ [0.8714, -0.0199, 264],
-#                            [0.2598,  0.6926, 103],
-#                            [0.0007, -0.0001, 1]])
-
-#P1 = (103,264)
-#P2 = (172,517)
-#P3 = (397,262) 
-#P4 = (388,520)
 P1 = (140,291)
 P2 = (188,497)
 P3 = (361,289) 
 P4 = (360,499)
 
-painting_height = painting_height - 1 #411
-painting_width = painting_width - 1 #504
+painting_height = painting_height - 1
+painting_width = painting_width - 1
 
 support = Polygon([P1, P2, P3, P4])
 painting_polygon = Polygon([(0,0),(0,painting_width),(painting_height,0),(painting_height,painting_width)])
@@ -82,43 +73,21 @@ multiplication = np.matmul(transformation, inverse)
 print multiplication
 
 
-
-
-#declaring support quadril
+#declaring support quadrilateral
 support = Polygon([P1, P2, P4, P3])
-
-
-print painting[0,0]
 
 for j in range(0, ambient_height - 1):
     for i in range(0, ambient_width - 1):
-        #print support.contains(point)
-#        #new_ambient[i, j] = painting[i, j]
-         point1 = Point(j, i)
-#        #print (i,j)
-#        #new_ambient[j,i] = [255,255,255]
-         correspondent = np.matmul(inverse, [j, i, 1])
-#        #print correspondent
-         point = Point(correspondent[1]/correspondent[2], correspondent[0]/correspondent[2])
-         if(support.contains(point1)):
-#            #print point
-#            #print math.floor(point.x), math.floor(point.y)
-             x = int(math.floor(point.x))
-             y = int(math.floor(point.y))
-             #if(x < 0):
-             #    x = 0
-             #elif x > painting_width:
-             #    x = painting_width
-             #if y < 0:
-             #    y = 0
-             #elif y > painting_height:
-             #    y = painting_height
-            #print x, y
-             new_ambient[j,i] = painting[y, x] 
-#            new_ambient[j,i] = 0
-#
-#cv2.imshow('original ambient', ambient)
-#cv2.imshow('painting', painting)
+        point1 = Point(j, i)
+        correspondent = np.matmul(inverse, [j, i, 1])
+        point = Point(correspondent[1]/correspondent[2], correspondent[0]/correspondent[2])
+        if support.contains(point1):
+            x = int(math.floor(point.x))
+            y = int(math.floor(point.y))
+            new_ambient[j, i] = painting[y, x]
+
+cv2.imshow('original ambient', ambient)
+cv2.imshow('painting', painting)
 cv2.imshow('new ambient', new_ambient)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
